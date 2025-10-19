05b22ad3e7d76dd3470e3d933530f7e72a77a99c
Christian Lührs <info@lohres-log.de>
2025-10-19 14:00:27 +0200

    chore: add RequestMethods.php

---
+++ src/Enums/RequestMethods.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace Lohres\RestService\Enums;
+
+use Exception;
+
+enum RequestMethods: string {
+    case GET = "GET";
+    case HEAD = "HEAD";
+    case POST = "POST";
+    case PUT = "PUT";
+    case DELETE = "DELETE";
+    case OPTIONS = "OPTIONS";
+    case PATCH = "PATCH";
+    case TRACE = "TRACE";
+    case CONNECT = "CONNECT";
+}
